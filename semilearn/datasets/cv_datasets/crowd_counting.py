8b4e1387596de7f6d4fee867e09492a70de444e7
Ben Temming <ben_temming@dd26102-vm.us-west1-a.c.dd2610-479117.internal>
2025-12-12 18:11:31 +0000

    fix: your fix

--- semilearn/datasets/cv_datasets/crowd_counting.py
+++ semilearn/datasets/cv_datasets/crowd_counting.py
@@ -120,6 +120,8 @@ class CROWD_COUNTING(VisionDataset):
         print(f"CrowdCounting dataset contains {num_samples} samples.")
 
         sample = self.__getitem__(0)
-        shape_image = sample[0].shape
+        sample = PIL.Image.open(self._file_paths[0]) #.convert("RGB")
+        sample = np.array(sample)
+        shape_image = sample.shape
         print(f"Image shape: {shape_image[0]}x{shape_image[1]}x{shape_image[2]}")
 
